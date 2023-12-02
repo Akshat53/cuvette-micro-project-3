7e8d70e9843beac9b1755652f3c28d6c64d50e2b
Akshat singh <singh.akshat.akshat26@gmail.com>
2023-12-02 23:05:48 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const foodData = require('./food.json');
- 
-function listFoodData(food){
-    if (food.length>0){
-        return foodData.filter((food)=>food.category === food)
-    }
-    else{
-        foodData
+const foodData = require("./food.json");
+
+function listFood(category = '') {
+    if (category.length > 0) {
+        const filteredFood = foodData.filter((food) => food.category === category);
+        console.log(`Food in category '${category}':`, filteredFood);
+        return filteredFood;
+    } else {
+        console.log('All food:', foodData);
+        return foodData;
     }
 }
-listFoodData('Protein');
-listFoodData('Nuts');
-listFoodData();
+
+listFood("Protein");
+listFood('Nuts');
+listFood();
+
+function sortCalorie(condition) {
+    let sortedFood = [];
+    if (condition === 'asec') {
+        sortedFood = foodData.sort((a, b) => a.calorie - b.calorie);
+        console.log('Sorted by calorie in ascending order:', sortedFood);
+    } else if (condition === "desc") {
+        sortedFood = foodData.sort((a, b) => b.calorie - a.calorie);
+        console.log('Sorted by calorie in descending order:', sortedFood);
+    }
+    return sortedFood;
+}
+
+sortCalorie('asec');
+sortCalorie('desc');
+
+function sort(type) {
+    let sortedFood = [];
+    if (type === "protein") {
+        sortedFood = foodData.sort((a, b) => b.proteins - a.proteins);
+        console.log('Sorted by protein content:', sortedFood);
+    } else if (type === "carb") {
+        sortedFood = foodData.sort((a, b) => a.cab - b.cab);
+        console.log('Sorted by carb content:', sortedFood);
+    }
+    return sortedFood;
+}
+
+sort('protein');
+sort('carb');
